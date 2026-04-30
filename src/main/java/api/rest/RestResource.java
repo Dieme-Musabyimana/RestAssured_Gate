7f7a5dafb50f400109405973c9aef3b44268d5fe
Dieme-Musabyimana <musabyimanadieme10@gmail.com>
2026-04-30 07:38:26 +0200

    Apply filters

--- src/main/java/api/rest/RestResource.java
+++ src/main/java/api/rest/RestResource.java
@@ -1,5 +1,6 @@
 package api.rest;
 
+import filters.CustomFilter;
 import io.restassured.response.Response;
 import utils.ConfigLoader;
 
@@ -13,18 +14,21 @@ public class RestResource {
 
     public static Response get(String path) {
         return given().spec(getRequestSpec())
+                .filter(new CustomFilter())
                 .when().get(path).then().spec(getResponseSpec()).extract().response();
     }
 
 
     public static Response get(String path, String UserId) {
         return given().spec(getRequestSpec())
+                .filter(new CustomFilter())
                 .pathParam("id",UserId)
                 .when().get(path).then().spec(getResponseSpec()).extract().response();
     }
 
     public static Response post(String path, Object payload) {
         return given().spec(getRequestSpec())
+                .filter(new CustomFilter())
                 .body(payload)
                 .when().post(path).
                 then().spec(getResponseSpec())
@@ -34,13 +38,15 @@ public class RestResource {
 
     public static Response put(String path, String paramName, Object paramValue, Object payload) {
         return given().spec(getRequestSpec())
+                .filter(new CustomFilter())
                 .pathParam(paramName, paramValue)
                 .body(payload)
-                .when().put(path);
+                .when().put(path).then().extract().response();
     }
 
     public static Response delete(String path, String paramName, Object paramValue) {
         return given().spec(getRequestSpec())
+                .filter(new CustomFilter())
                 .pathParam(paramName, paramValue)
                 .when().delete(path);
     }
@@ -48,6 +54,7 @@ public class RestResource {
 
         return given()
                 .baseUri(ConfigLoader.getEchoUrl())
+                .filter(new CustomFilter())
                 .multiPart("file", file)
                 .post(path).
                 then().spec(getPostManResponseSpec())
@@ -55,6 +62,7 @@ public class RestResource {
     }
     public static Response encodedTest (String path, String paramName, Object value  ){
         return given().spec(getPostManRequestSpec())
+                .filter(new CustomFilter())
                 .formParam(paramName,value)
                 .when()
                 .post( path)
